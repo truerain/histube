09d4ad1ed2e755cf5cabe390f2d1e4580b4681fd
Y.W Shin <yunwoo.shin@gmail.com>
2023-11-28 13:17:25 +0000

    Update 6 files

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import { FaBars } from 'react-icons/fa'
-import { AiOutlineSearch } from 'react-icons/ai'
-import { MdNotifications, MdApps } from 'react-icons/md'
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdNotifications, MdApps } from "react-icons/md";
 
-import './_header.scss'
+import "./_header.scss";
 
 const Header = () => {
   return (
     <div className="header">
-      <FaBars className="header__menu"/>
-      <img src="http://pngimg.com/uploads/youtube/youtube_PNG2.png" 
-           alt=""
-           className="header__logo"
+      <FaBars className="header__menu" />
+      <img
+        src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+        alt=""
+        className="header__logo"
       />
       <form>
         <input type="text" placeholder="Search" />
-        <button type="submit"> <AiOutlineSearch size={22}/> </button>
+        <button type="submit">
+          {" "}
+          <AiOutlineSearch size={22} />{" "}
+        </button>
       </form>
 
       <div className="header__icons">
@@ -24,10 +28,11 @@ const Header = () => {
         <img
           className="avatar"
           src="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+          alt="avatar"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import './_sidebar.scss'
+import React from "react";
+
+import {
+  MdExitToApp,
+  MdHistory,
+  MdHome,
+  MdLibraryBooks,
+  MdSentimentDissatisfied,
+  MdSubscriptions,
+  MdThumbUp,
+} from "react-icons/md";
+import "./_sidebar.scss";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      Sidebar
-    </div>
-  )
-}
+    <nav className="sidebar">
+      <li>
+        <MdHome size={23} />
+        <span>Home</span>
+      </li>
+      <li>
+        <MdSubscriptions size={23} />
+        <span>Subscriptions</span>
+      </li>
+      <li>
+        <MdThumbUp size={23} />
+        <span>Like Video</span>
+      </li>
+      <li>
+        <MdHistory size={23} />
+        <span>History</span>
+      </li>
+      <li>
+        <MdLibraryBooks size={23} />
+        <span>Library</span>
+      </li>
+      <li>
+        <MdSentimentDissatisfied size={23} />
+        <span>I don't know</span>
+      </li>
+      <hr />
 
-export default Sidebar
+      <li>
+        <MdExitToApp size={23} />
+        <span>Log out</span>
+      </li>
+
+      <hr />
+    </nav>
+  );
+};
+
+export default Sidebar;
